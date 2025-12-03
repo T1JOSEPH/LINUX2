941ea93603b47ab8cfeb59ee1a0d37a005ab95ee
T1JOSEPH <jovacame@uide.edu.ec>
2025-12-03 15:52:48 +0000

    Mi segundo script2 actualizado

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
-uname -a #muestra toda la información del sistema, como el tipo de kernel, la versión y el nombre del equipo.
-lscpu #muestra la información del procesador, como núcleos, arquitectura y velocidad.
-free -h #muestra cuánta memoria RAM está en uso y cuánta está libre, en un formato fácil de leer.
-df -h #muestra el espacio disponible y usado en los discos, en un formato fácil de entender.
+uname -a #Nos muestra toda la información del sistema, como el tipo de kernel, la versión y el nombre del equipo.
+lscpu #Nos enseña la información del procesador, como núcleos, arquitectura y velocidad.
+free -h #Muestra cuánta memoria RAM está en uso y cuánta está libre, en un formato fácil de leer.
+df -h #Muestra el espacio disponible y usado en los discos, en un formato fácil de entender.
