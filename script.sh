a562f5778d0bad13a0f5235766d79193e479e9df
T1JOSEPH <jovacame@uide.edu.ec>
2025-12-03 15:46:30 +0000

    Actualizado script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 # Crear y listar archivos
 echo "Hola linux" > archivo.txt #Este comando crea el archivo archivo.txt y guarda el texto Hola linux dentro, reemplazando lo que tenía antes
-cat archivo.txt
-cp arrchivo.txtchivo.txt copia.txt
-ls -l
-rm archivo.txt
+cat archivo.txt #Muestra en la pantalla todo lo que contiene el archivo archivo.txt
+cp arrchivo.txtchivo.txt copia.txt #Nos ayuda a copiar el archivo archivo.txt y crear otro llamado copia.txt con el mismo contenido.
+ls -l # Nos muestra los archivos y carpetas con detalles, como permisos, dueño, tamaño y fecha.
+rm archivo.txt # sirve para borrar el archivo archivo.txt de forma permanente.
