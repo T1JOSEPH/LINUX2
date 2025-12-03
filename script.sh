1a03d00f9677e8d0ae42a15ad8a1739c65e66eea
T1JOSEPH <jovacame@uide.edu.ec>
2025-12-03 15:41:44 +0000

    Mi primer script

---
+++ script.sh
@@ -0,0 +1,6 @@
+# Crear y listar archivos
+echo "Hola linux" > archivo.txt #Este comando crea el archivo archivo.txt y guarda el texto Hola linux dentro, reemplazando lo que tenía antes
+cat archivo.txt
+cp arrchivo.txtchivo.txt copia.txt
+ls -l
+rm archivo.txt
